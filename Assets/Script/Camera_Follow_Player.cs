60957b98ac8a097d85fc9846c2ce7e3c08626803
Vlad Bronowicki <w.i.bronowicki@gmail.com>
2016-04-06 00:15:46 -0400

    Added Functionality to Camera Script
    Signed-off-by: Vlad Bronowicki <w.i.bronowicki@gmail.com>

--- Assets/Script/Camera_Follow_Player.cs
+++ Assets/Script/Camera_Follow_Player.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class Camera_Follow_Player : MonoBehaviour {
 	public GameObject camHolder;
 
-	public float zPos = -5;
-	Vector3 playerPosition;
+	private float zPos = -5;
+	private Vector3 playerPosition;
+    private float upperY = 17.5f;
+    private float lowerY = -4.0f;
 	// Use this for initialization
 	void Start () {
 		playerPosition = new Vector3 ();
@@ -14,10 +16,26 @@ public class Camera_Follow_Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		playerPosition.x = this.transform.position.x;
-		playerPosition.y = this.transform.position.y;
+        if (this.transform.position.y > lowerY && this.transform.position.y < upperY)
+        {
+            playerPosition.y = this.transform.position.y;
+            Debug.Log("between");
+        }
+        else
+        {
+            Debug.Log("Below or above");
+            if (this.transform.position.y < lowerY)
+            {
+                playerPosition.y = lowerY;
+            }
+            else if (this.transform.position.y > upperY)
+            {
+                playerPosition.y = upperY;
+            }
+        }
 		playerPosition.z = zPos;
 		camHolder.transform.position = playerPosition;
-		Debug.Log (playerPosition);
+		//Debug.Log (playerPosition);
 
 	}
 }
