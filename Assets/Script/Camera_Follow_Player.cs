eb42b624b430b77521814689c220f190b14e5ff9
Vlad Bronowicki <w.i.bronowicki@gmail.com>
2016-04-07 21:40:38 -0400

    Reverted to first commit where files were added

---
+++ Assets/Script/Camera_Follow_Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Follow_Player : MonoBehaviour {
+	public GameObject camHolder;
+
+	public float zPos = -5;
+	Vector3 playerPosition;
+	// Use this for initialization
+	void Start () {
+		playerPosition = new Vector3 ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerPosition.x = this.transform.position.x;
+		playerPosition.y = this.transform.position.y;
+		playerPosition.z = zPos;
+		camHolder.transform.position = playerPosition;
+		Debug.Log (playerPosition);
+
+	}
+}
