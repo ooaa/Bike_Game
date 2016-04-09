658daf41f3f24459ea1ffddd0738f75c6cc717b1
Vlad Bronowicki <w.i.bronowicki@gmail.com>
2016-04-09 17:41:48 -0400

    Changes to Cemera Script, added Player Reset Script to player, and addrd player jump script (WiP)

---
+++ Assets/Script/Player_Jump.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Jump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Debug.Log("TOUCH!");
+        }
+
+        
+	}
+}
