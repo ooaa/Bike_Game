11fbbb3d408cbf55b723a8763049e8adbe30e08b
ooaa <conniechoi94@gmail.com>
2016-04-13 02:08:58 -0400

    Got the score menu working, start button still doesnt load level_1

---
+++ Assets/Script/Start_Menu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine;
+
+public class Start_Menu : MonoBehaviour {
+
+	public void PlayOn() 
+	{
+		SceneManager.LoadScene (0);
+	}
+}
